6bc6775efc5d3c205fabd59ed0d294de321274a1
setomulyovic <32457267+setomulyovic@users.noreply.github.com>
2017-10-23 18:37:33 +0700

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp.pkg6;
-
-/**
- *
- * @author DELL
- */
 
 public class Circle implements Shape {
     private double radius;
